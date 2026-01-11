ebcee056ceff4e0970832de0d6f2a697e0371fd4
Serhii Pokhodenko <nachkar.santyago@gmail.com>
2026-01-11 15:12:06 -0500

    Refactor modal functionality and enhance button styles; add close icon and update HTML structure

--- js/modal.js
+++ js/modal.js
@@ -6,3 +6,17 @@ const toggleModal = () => modal.classList.toggle('is-hidden');
 
 modalBtnOpen.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
+
+// Закриття Modal при кліку на фон
+modal.addEventListener('click', event => {
+  if (event.target === modal) {
+    toggleModal();
+  }
+});
+
+// Закриття Modal при натиску на esc
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape' && !modal.classList.contains('is-hidden')) {
+    toggleModal();
+  }
+});
